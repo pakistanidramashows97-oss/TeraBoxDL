aee869455c5a4c432d40305098656697f928316d
kingak971916 <kingak971916@gmail.com>
2026-03-08 09:28:06 +0530

    Update __init__.py

--- TeraBoxDownloader/__init__.py
+++ TeraBoxDownloader/__init__.py
@@ -71,7 +71,7 @@ try:
     aria2 = aria2p.API(
         aria2p.Client(
             host="http://localhost",
-            port=8080,
+            port=6800,
             secret=Var.ARIA2_SECRET
         )
     )
